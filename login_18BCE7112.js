be679b2a2a6edde179cdf5fd77195a2a2eb5a2c4
siddu6003 <51458421+siddu6003@users.noreply.github.com>
2021-03-10 11:43:01 +0530

    Add files via upload

---
+++ login_18BCE7112.js
@@ -0,0 +1,7 @@
+function submit(){
+ let x=document.getElementById('pass')
+ let y=document.getElementById('pass').placeholder
+ let z=document.getElementById('capin').innerHTML;
+let A=document.getElementById('captcha').innerHTML;
+ let l=x.value.length;
+}
